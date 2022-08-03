e06224b652ac64c0078f3320a73361c0fbfa8581
mnwana <mariellenwana@gmail.com>
2022-08-02 22:53:54 -0400

    create generate page module

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const profileDataArgs = process.argv.slice(2,process.argv.length);
+const fs = require("fs");
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-   profileDataArr.forEach(profileItem => 
-    console.log(profileItem)
-   );
-}
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 
-printProfileData(profileDataArgs);
+const [name, github] = profileDataArgs;
 
+fs.writeFile(`index.html`, generatePage(name, github), (err) => {
+  if (err) throw err;
+  console.log("File created at index.html");
+});
